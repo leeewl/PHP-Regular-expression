6e20653011fc1fe227ac7ddb2ba1cb032169140c
leeewl <33928880+leeewl@users.noreply.github.com>
2017-12-20 19:12:33 +0800

    Add files via upload
    测试用例

---
+++ tagsToOrg/tags.el
@@ -0,0 +1,20 @@
+(setq  g-ac-php-tmp-tags  [
+(
+  ("\\Class1".[
+    ["d" "AFTER" ""  "sys"  "void" "\\Redis" "public"  ]
+    ["d" "PIPELINE" ""  "sys"  "void" "\\Redis" "public"  ]
+    ["d" "REDIS_HASH" ""  "sys"  "void" "\\Redis" "public"  ]
+    ["m" "__construct(" ""  "sys"  "" "\\Redis" "public"  ]
+    ["m" "connect(" "$host, $port=6379, $timeout=0"  "sys"  "bool" "\\Redis" "public"  ]
+    ["m" "get(" "$key"  "sys"  "string|bool" "\\Redis" "public"  ]
+    ["m" "__get(" "$dbname"  "sys"  "MongoDB" "\\MongoClient" "public"  ]
+    ["m" "getConnections(" ""  "sys"  "array" "\\MongoClient" "public"  ]
+  ("\\Class2".[
+    ["m" "__construct(" ""  "sys"  "" "\\SplDoublyLinkedList" "public"  ]
+    ["m" "add(" "$index, $newval"  "sys"  "void" "\\SplDoublyLinkedList" "public"  ]
+    ["m" "bottom(" ""  "sys"  "mixed" "\\SplDoublyLinkedList" "public"  ]
+    ["m" "isEmpty(" ""  "sys"  "bool" "\\SplDoublyLinkedList" "public"  ]
+    ["m" "key(" ""  "sys"  "mixed" "\\SplDoublyLinkedList" "public"  ]
+    ["m" "next(" ""  "sys"  "void" "\\SplDoublyLinkedList" "public"  ]
+]
+])
